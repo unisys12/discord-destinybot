fbcfc2b3b9f057ec1e37d08ab3ad3aa6c2cadb07
Phillip Jackson <unisys12@gmail.com>
2016-08-16 23:35:10 -0500

    basic read write streams setup but errors when writting to file

--- bot/commands/manifest.js
+++ bot/commands/manifest.js
@@ -3,7 +3,11 @@
 var co = require('co');
 var util = require('util');
 var logger = require('winston');
+var thunkify = require('thunkify');
+var request = require('request');
+var get = thunkify(request.get); 
 var fs = require('fs');
+
 
 function exec(cmd) {
 
@@ -17,13 +21,24 @@ function exec(cmd) {
         var worldContents = JSON.stringify(manifest);
         var mobileWorldContents = JSON.parse(worldContents);
                 
-        var res = (yield get(mobileWorldContents.mobileWorldContentPaths.en))[0];
-        
+        var res = (yield get("http://www.bungie.net" + mobileWorldContents.mobileWorldContentPaths.en))[0];
+        logger.debug("Retrieveing Mobile World Contents from ... http://www.bungie.net" + mobileWorldContents.mobileWorldContentPaths.en[0]);
+        //yield cmd.destiny.mobileWorldContents(res);
+
         if (res.statusCode !== 200) {
             logger.error("failed to download mobileWorldContent: %s\n", res.statusCode);
             throw new Error("download failure: "+res.statusMessage);
         } else {
-            var zip = res.body;
+            logger.debug("Reading manifest file...");
+            var file = fs.createReadStream(res.body, 'hex');
+            logger.debug("Preparing to write to worldcontents.zip...")
+            var zip = fs.createWriteStream(__dirname + "/../../persist/worldcontents.zip");
+            
+            file.pipe(zip);
+            
+            logger.debug("Contents written to worldcontents.zip...")
+            // check the file if it was properly written
+
             // unzip the DB
         }
 
